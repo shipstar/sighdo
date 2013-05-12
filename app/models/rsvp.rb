16f71e1469b65bc564df575f3e98df69ca75c5f5
Kyle Shipley <kyleashipley@gmail.com>
2013-05-11 21:05:20 -0400

    Add checkboxes for Friday/Saturday/Sunday.

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -2,6 +2,18 @@ class Rsvp
   include Mongoid::Document
   field :email, type: String
   field :guests, type: Integer
-  field :dietary_restrictions, type: String
-  field :details, type: String
+  field :friday, type: Boolean, default: false
+  field :saturday, type: Boolean, default: false
+  field :sunday, type: Boolean, default: false
+
+  def attending
+    return 'All' if friday && saturday && sunday
+    return 'None' unless friday || saturday || sunday
+
+    ret = []
+    ret << 'Friday' if friday
+    ret << 'Saturday' if saturday
+    ret << 'Sunday' if sunday
+    ret.join(', ')
+  end
 end
